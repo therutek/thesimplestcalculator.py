eb51e36c7eb1ba5d0f3db8b33df4033d4383128c
therutek <danielrutkowski31@gmail.com>
2025-04-02 18:31:35 +0200

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,12 @@
 print("Witaj w kalkulatorze!")
 
-liczba1 = float(input("Podaj pierwszą liczbę: "))                 # Float konwertuje tekst na liczbę zmiennoprzecinkową, np. 3, 14.2, 1.24 itd.
+liczba1 = float(input("Podaj pierwszą liczbę: "))                 # Float konwertuje tekst na liczbę zmiennoprzecinkową, np. 3.11, 14.2, 1.24 itd.
 liczba2 = float(input("Podaj drugą liczbę: "))
+liczba3 = input("Czy chcesz podać trzecią liczbę? (T/N) - (tak/nie): ")
+if liczba3 == "T" or liczba3 == "t" or liczba3 == "tak" or liczba3 == "Tak":
+    liczba3 = float(input("Podaj trzecią liczbę: "))
+elif liczba3 == "N" or liczba3 == "n" or liczba3 == "nie" or liczba3 == "Nie":
+    liczba3 = None
 operacja = input("Wybierz operację (+, -, *, /): ")
 
 if operacja == "+":
@@ -21,9 +26,14 @@ else:
 print("Wynik:", wynik)
 
 kolejna = input("Czy chcesz wykonać kolejne obliczenia? (tak/nie): ")
-while kolejna == "tak":           # Pętla "while" pozwala na wielokrotne wykonywanie tego samego kodu
+while kolejna == "tak":                       # Pętla "while" pozwala na wielokrotne wykonywanie tego samego kodu
     liczba1 = float(input("Podaj pierwszą liczbę: "))
     liczba2 = float(input("Podaj drugą liczbę: "))
+    liczba3 = input("Czy chcesz podać trzecią liczbę? (T/N) - (tak/nie): ")
+if liczba3 == "T" or liczba3 == "t" or liczba3 == "tak" or liczba3 == "Tak":
+    liczba3 = float(input("Podaj trzecią liczbę: "))
+elif liczba3 == "N" or liczba3 == "n" or liczba3 == "nie" or liczba3 == "Nie":
+    liczba3 = None
     operacja = input("Wybierz operację (+, -, *, /): ")
 
     if operacja == "+":
